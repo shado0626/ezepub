4a4311dbb8e91d973d7e95a064adc68a8d4ee21a
Sherin Binu <sherinbinu@hotmail.com>
2021-08-02 15:20:41 +0530

    read file and folder names

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
-console.log('Hello world')
+import path from 'path'
+
+console.log(`Filename: ${require?.main?.filename}`)
+console.log(`Foldername: ${path.dirname(require?.main?.filename as string)}`)
